e83e77af29b66cce2f683dce879c8b65628af7e7
amerlin <andrea.merlin@aalto.it>
2023-03-23 17:31:51 +0100

    second commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Demo003.Startup))]
+namespace Demo003
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
